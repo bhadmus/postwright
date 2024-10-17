62bccf7feef8b68698d983b57a4e048c0fa4bc01
Ademola Bhadmus <sanguine@Ademolas-MBP.lan>
2024-10-17 21:47:58 +0100

    removed logic

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const packageJson = require('./package.json');
 
 program
   .version(packageJson.version)
-  .description('Convert Postman collections to Playwright test scripts');
+  .description('Convert Postman collections to Playwright test scripts')
+  .option('-c, --convert <postmanCollectionPath>', 'Convert Postman collection to Playwright scripts')
+  .option('-o, --output <outputDir>', 'Output directory for Playwright scripts', process.cwd())
+  .action((options) => {
+    if (options.convert) {
+      const postmanCollectionPath = path.resolve(options.convert);
+      const outputDir = path.resolve(options.output);
+      convertPostmanToPlaywright(postmanCollectionPath, outputDir);
+    }
+  });
 
 program
   .command('convert <postmanCollectionPath>')
@@ -17,3 +26,8 @@ program
   });
 
 program.parse(process.argv);
+
+// If no arguments are provided, show help
+if (!process.argv.slice(2).length) {
+  program.outputHelp();
+}
